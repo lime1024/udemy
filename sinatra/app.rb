561bc88c6582fd66fe932173d95929e0d4812324
lime1024 <lime1024@gmail.com>
2018-09-05 17:52:44 +0900

    First commit "1 - 21"

---
+++ sinatra/app.rb
@@ -0,0 +1,33 @@
+require 'bundler'
+
+Bundler.require
+
+set :database, {adapter: "sqlite3", database: "contacts.sqlite3"}
+
+class Contact < ActiveRecord::Base
+  validates_presence_of :name
+end
+
+get '/' do
+  @now = Time.now
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contact_new' do
+  @contact = Contact.new
+  erb :contact_form
+end
+
+post '/contacts' do
+  name = params[:name]
+
+  @contact = Contact.new({name: name})
+  if @contact.save
+    # true
+    redirect '/'
+  else
+    # false
+    erb :contact_form
+  end
+end
