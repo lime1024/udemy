1111abd36ec5a5f3d73c61f2d738eaa0152f47d3
lime1024 <lime1024@gmail.com>
2018-09-20 17:25:26 +0900

    End of section12 "61-69"

--- railstodo/db/schema.rb
+++ railstodo/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_090102) do
+ActiveRecord::Schema.define(version: 2018_09_20_065535) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -19,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_09_14_090102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "priority", default: 0, null: false
+    t.integer "category_id"
   end
 
 end
